5cfb4518d2fbc44687b8c5ddf94b69d9fe232d07
steve <steve.nguyen2013@gmail.com>
2022-05-29 16:16:31 -0700

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  return (
+    <button
+      onFocus={(focus) => console.log(`Good!`)}
+      onBlur={(blur) => console.log(`Hey! Eyes on me!`)}
+    >
+      Eyes on me
+    </button>
+  );
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+
+import React from "react";
+
+function Keypad() {
+  function change(event) {
+    console.log(`Entering ${event.target.type}...`);
+  }
+  return (
+    <div>
+      <input type="password" onChange={change}></input>
+    </div>
+  );
+}
+
+export default Keypad;
